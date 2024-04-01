6501de980e2e13d121b5eae659e4b69af3e532ad
faizanhemotra <faizanessex@gmail.com>
2024-04-01 17:40:45 +0100

    Search on Google button for out of stock products Item model number is preferred for google search

--- contentScript.js
+++ contentScript.js
@@ -1,15 +1,96 @@
 // contentScript.js
 
 // Function to find the product title
-function findTitle() {
+function findPlacementForWidget() {
+  // the last two selectors are for mobile devices
   return document.querySelector(
     "div#centerCol, div#dp.ce_mobile, div#productTitleGroupAnchor"
+  );
+}
+
+function productTitle() {
+  return document.querySelector("span#productTitle").innerText;
+}
+
+// Function to check if the current page URL matches the specified format
+function isProductPage() {
+  return /\/dp\/[A-Z0-9]{10}/.test(window.location.href);
+}
+
+function findoutOfStock() {
+  // second selector is for mobile devices
+  return document.querySelector(
+    "div#outOfStock, div#pwAvailabilityExclusion_feature_div"
+  );
+}
+
+function findbuybox() {
+  // second selector is for mobile devices
+  return document.querySelector(
+    "div#buybox, div#pwAvailabilityExclusion_feature_div"
   );
 }
 
 // Function to check if the current page URL matches the specified format
 function isProductPage() {
   return /\/dp\/[A-Z0-9]{10}/.test(window.location.href);
+}
+
+function findItemModelNumberFromProductDetailsList() {
+  const ulElements = document.querySelectorAll(
+    "#detailBullets_feature_div > ul"
+  );
+  for (const ulElement of ulElements) {
+    const listItems = ulElement.querySelectorAll("li");
+    for (const listItem of listItems) {
+      const text = listItem.innerText;
+      const regex = /Item\s*model\s*number\s*:\s*(.+)/i;
+      const match = text.match(regex);
+      if (match && match[1]) {
+        return match[1];
+      }
+    }
+  }
+  return null;
+}
+
+// Function to get Model Number/Part Number
+function findModelNumberFromProdDetails() {
+  const technicalSpecificationsSection = document.querySelector(
+    "#technicalSpecifications_feature_div, #prodDetails"
+  );
+  if (technicalSpecificationsSection) {
+    const tableRows = technicalSpecificationsSection.querySelectorAll("tr");
+    let itemModelNumber = "";
+    let modelNumber = "";
+    let partNumber = "";
+    tableRows.forEach((row) => {
+      const th = row.querySelector("th");
+      if (
+        th &&
+        th.textContent.trim().toLowerCase().includes("item model number")
+      ) {
+        const td = row.querySelector("td");
+        if (td) {
+          itemModelNumber = td.textContent.trim();
+        }
+      }
+      if (th && th.textContent.trim().toLowerCase().includes("model number")) {
+        const td = row.querySelector("td");
+        if (td) {
+          modelNumber = td.textContent.trim();
+        }
+      }
+      if (th && th.textContent.trim().toLowerCase().includes("part number")) {
+        const td = row.querySelector("td");
+        if (td) {
+          partNumber = td.textContent.trim();
+        }
+      }
+    });
+    return itemModelNumber || partNumber || modelNumber || null;
+  }
+  return null;
 }
 
 // Function to create the widget container
@@ -64,10 +145,48 @@ function handleButtonClick(dropdown) {
   };
 }
 
+function createGoogleSearchButton(modelNumber) {
+  const button = document.createElement("button");
+  button.textContent = "Search on Google";
+  button.title = "Search for the product on Google";
+  button.style.padding = "6px 12px";
+  button.style.fontSize = "14px";
+  button.style.cursor = "pointer";
+  button.addEventListener("click", () => {
+    const searchUrl = `https://www.google.com/search?q="${modelNumber}"`;
+    window.open(searchUrl, "_blank");
+  });
+  return button;
+}
+
+function addGoogleSearch() {
+  const outOfStockElement = findoutOfStock();
+  if (outOfStockElement) {
+    const buyboxElement = findbuybox();
+    if (buyboxElement) {
+      // it's difficult to scrape model number on non-english pages, hence using the product title instead
+      // Also if there is no number specified we fallback to the product title
+      const itemModelNumber =
+        findItemModelNumberFromProductDetailsList() ||
+        findModelNumberFromProdDetails() ||
+        productTitle();
+      if (itemModelNumber) {
+        const buttonContainer = document.createElement("div");
+        buttonContainer.style.display = "flex";
+        buttonContainer.style.justifyContent = "center";
+        buttonContainer.style.marginTop = "5px";
+        const googleSearchButton = createGoogleSearchButton(itemModelNumber);
+        buttonContainer.appendChild(googleSearchButton);
+        buyboxElement.after(buttonContainer);
+      }
+    }
+  }
+}
+
 // Function to create the widget and its components
 function createWidget() {
   if (!isProductPage()) return; // Exit if not a product page
-  const productTitle = findTitle();
+  const placementForWidget = findPlacementForWidget();
   const amazonFronts = [
     { name: "🇨🇦 CA", hostname: "www.amazon.ca" },
     { name: "🇯🇵 CO.JP", hostname: "www.amazon.co.jp" },
@@ -101,7 +220,8 @@ function createWidget() {
   widget.appendChild(dropdown);
   widget.appendChild(button);
 
-  if (!productTitle) {
+  // if we can't find the selectors indicating a live product page it's most likely a 404 page
+  if (!placementForWidget) {
     if (centerTable !== null) {
       centerTable.parentElement.insertBefore(widget, centerTable);
     } else if (dogsofAmazonLink !== null) {
@@ -109,9 +229,10 @@ function createWidget() {
       parentElement.parentNode.insertBefore(widget, parentElement);
     }
   } else {
-    productTitle.prepend(widget);
+    placementForWidget.prepend(widget);
   }
 }
 
 // Call the function to create the widget
 document.addEventListener("DOMContentLoaded", createWidget());
+document.addEventListener("DOMContentLoaded", addGoogleSearch());
